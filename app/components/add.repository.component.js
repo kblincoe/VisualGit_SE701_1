4cb392342aea008dd0045b99c8c76f59e4b1e0a5
Austin Sutherland <asut324@aucklanduni.ac.nz>
2018-03-18 21:52:07 +1300

    stop adding a repo if no text entered
    fixed js exports
    
    updated to work with buttons
    
    fixed triple equals comparisons

--- app/components/add.repository.component.js
+++ app/components/add.repository.component.js
@@ -5,20 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
+
 var core_1 = require("@angular/core");
-var AddRepositoryComponent = (function () {
+var AddRepositoryComponent = /** @class */ (function () {
     function AddRepositoryComponent() {
     }
     AddRepositoryComponent.prototype.addRepository = function () {
-        downloadRepository();
-        switchToMainPanel();
+        if (document.getElementById("repoSave").value === "") {
+            displayModal("Please enter a local path!");
+        }
+        else if (document.getElementById("repoClone").value === "") {
+            displayModal("Please enter a remote URL!");
+        }
+        else if (document.getElementById("repoSaveLocation").value === "") {
+            displayModal("Please enter a save location!");
+        }
+        else {
+            downloadRepository();
+            switchToMainPanel();
+        }
     };
     AddRepositoryComponent.prototype.openRepository = function () {
-        openRepository();
-        switchToMainPanel();
+        if (document.getElementById("repoOpen").value === "") {
+            displayModal("Please enter a local path!");
+        }
+        else {
+            openRepository();
+            switchToMainPanel();
+        }
     };
     AddRepositoryComponent.prototype.returnToMainPanel = function () {
         switchToMainPanel();
